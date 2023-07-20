59da0e7c1cf116989ef973cc7114b44ca31f550a
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-07-20 19:39:24 +0800

    feat(common-util):新增FileUploadUtil工具类用于用户上传头像。

--- common/common-util/src/main/java/feidian/util/serviceUtil/FileUploadUtil.java
+++ common/common-util/src/main/java/feidian/util/serviceUtil/FileUploadUtil.java
@@ -1,15 +1,78 @@
 package feidian.util.serviceUtil;
 
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.UUID;
+
 public class FileUploadUtil {
 
-    public static void uploadAvatar(){
+    // 支持的图片文件格式
+    private static final String[] SUPPORTED_IMAGE_EXTENSIONS = {"jpg", "jpeg", "png"};
 
+    public static String uploadAvatar(MultipartFile avatarFile){
+
+        if (avatarFile.isEmpty()) {
+            return "未选择文件";
+        }
+
+        //根据文件拓展名判断文件是否合法
+        String fileExtension = getFileExtension(avatarFile.getOriginalFilename());
+        boolean isValidExtension = false;
+        for (String allowedFileFormat : Arrays.asList(SUPPORTED_IMAGE_EXTENSIONS)){
+            if (fileExtension.equals(allowedFileFormat)){
+                isValidExtension = true;
+                break;
+            }
+        }
+        if(!isValidExtension){
+            return "文件格式不支持";
+        }
+
+        //文件保存在服务器上的目录地址
+        String uploadDir = "http://182.254.242.96:3333//www//wwwroot//FeidianOfficialWebsite2023//image//";
+
+        try {
+            // 定义保存路径
+            // 创建保存目录（如果不存在）
+            File dir = new File(uploadDir);
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+
+            // 生成保存文件的路径，利用UUID生成唯一标识
+            String filePath = uploadDir + UUID.randomUUID() + avatarFile.getOriginalFilename();
+
+            // 保存文件
+            avatarFile.transferTo(new File(filePath));
+            return filePath;
+
+        } catch (IOException e) {
+            return "上传失败" + e.getMessage();
+        }
     }
 
-    public static void displayAvatar(){
 
+    //获取文件拓展名
+    public static String getFileExtension(String fileName) {
+        String extension = "";
+
+        // 检查文件路径是否为空
+        if (fileName != null && !fileName.isEmpty()) {
+            // 获取最后一个点的索引，表示文件名和扩展名的分隔位置
+            int dotIndex = fileName.lastIndexOf('.');
+
+            // 检查是否有有效的扩展名
+            if (dotIndex >= 0 && dotIndex < fileName.length() - 1) {
+                // 提取扩展名部分
+                extension = fileName.substring(dotIndex + 1);
+            }
+        }
+        return extension;
     }
-
 
 
 }
