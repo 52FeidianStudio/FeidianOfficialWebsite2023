8fc4a93ce7eda54e8f7ef77fa008f6982c5a5572
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-07-21 19:32:49 +0800

    feat(model):完善了SectionalRegisterVO

--- model/src/main/java/com/feidian/vo/SectionalRegisterVO.java
+++ model/src/main/java/com/feidian/vo/SectionalRegisterVO.java
@@ -10,4 +10,20 @@ import org.springframework.stereotype.Component;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SectionalRegisterVO {
+
+    private Long userId;
+    private Long registerId;
+
+    private String name;
+    private String nickname;
+    private String avatarUrl;
+
+    private Long subjectId;
+    private String gradeName;
+
+    //想要申请的组别
+    private Long desireDepartmentId;
+    //申请状态(0表示已提交，1表示已查看，2表示已通过，3表示未通过)
+    private String status;
+
 }
