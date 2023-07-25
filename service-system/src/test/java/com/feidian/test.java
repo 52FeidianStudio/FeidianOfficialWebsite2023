aaa623655dfc3df40d378ac6f239891c3190c8bc
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-07-25 15:04:30 +0800

    chore(service-system):转移了registerController的位置

--- service-system/src/test/java/com/feidian/test.java
+++ service-system/src/test/java/com/feidian/test.java
@@ -1,5 +1,7 @@
 package com.feidian;
 
+import com.feidian.util.serviceUtil.EmailUtil;
+import com.feidian.util.serviceUtil.VerifyCode;
 import org.junit.jupiter.api.Test;
 
 
@@ -9,5 +11,11 @@ public class test {
     public void test(){
         String str = "$2a$10$OgBm1.98uj.9TznI6p/jsuXOjXjmgfirTRukyxMNHPguPYQ0.EjSC(String)";
         System.out.println(str.length());
+    }
+
+    @Test
+    public void test02(){
+        String str = VerifyCode.setVerifyCode();
+        EmailUtil.sendEmail("2695946524@qq.com",str);
     }
 }
