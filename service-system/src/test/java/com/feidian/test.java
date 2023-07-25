990caf4bceed91df5a92e5738d6e5c779a792458
Kannen <qkn0208@webmail.hzau.edu.cn>
2023-07-25 10:01:03 +0800

    feat(service-system):新增单元测试，开始利用ApiFox进行测试

---
+++ service-system/src/test/java/com/feidian/test.java
@@ -0,0 +1,13 @@
+package com.feidian;
+
+import org.junit.jupiter.api.Test;
+
+
+public class test {
+
+    @Test
+    public void test(){
+        String str = "$2a$10$OgBm1.98uj.9TznI6p/jsuXOjXjmgfirTRukyxMNHPguPYQ0.EjSC(String)";
+        System.out.println(str.length());
+    }
+}
