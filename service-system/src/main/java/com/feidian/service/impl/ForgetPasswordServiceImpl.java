fe35a49359cb78f5012b7d60b9f9ca576a77cb25
Rudius <3424769799@qq.com>
2023-07-25 11:35:10 +0800

    pass

--- service-system/src/main/java/com/feidian/service/impl/ForgetPasswordServiceImpl.java
+++ service-system/src/main/java/com/feidian/service/impl/ForgetPasswordServiceImpl.java
@@ -6,35 +6,28 @@ import com.feidian.exception.SystemException;
 import com.feidian.mapper.ForgetPasswordMapper;
 import com.feidian.mapper.UserMapper;
 import com.feidian.responseResult.ResponseResult;
-import com.feidian.service.FacultyService;
 import com.feidian.service.ForgetPasswordService;
+import com.feidian.util.RedisCache;
+import com.feidian.util.serviceUtil.EmailUtil;
 import com.feidian.util.serviceUtil.VerifyCode;
 import io.micrometer.core.instrument.util.StringUtils;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.TimeUnit;
+
 
 @Service
 @Slf4j
 public class ForgetPasswordServiceImpl implements ForgetPasswordService {
-    @Value("${spring.mail.username}")
-    private String from;   // 邮件发送人
 
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisCache redisCache;
     @Autowired
     private ForgetPasswordMapper forgetPasswordMapper;
     @Autowired
     private UserMapper userMapper;
-    @Autowired
-    private JavaMailSender mailSender;
+
     @Override
     public ResponseResult forgetPassword(ForgetPasswordDTO forgetPasswordDTO) {
 
@@ -56,30 +49,15 @@ public class ForgetPasswordServiceImpl implements ForgetPasswordService {
         if(!email.equals(address)){
             throw new SystemException(HttpCodeEnum.USERNAME_EMAIL_NOT_MATCH);
         }
-        //返回验证码
-        return sendMsg(address);
+        String code = VerifyCode.setVerifyCode();
+
+        redisCache.setCacheObject("forget:" + address,code);
+        EmailUtil.sendEmail(address,code);
+        return ResponseResult.successResult();
     }
 
-    public ResponseResult sendMsg(String address) {
-        String subject = "忘记密码邮箱验证码";
-        if (StringUtils.isNotEmpty(address)) {
-            String code = VerifyCode.setVerifyCode();
-            String context = "您的验证码验证码为: " + code + ",五分钟内有效，请妥善保管!";
-            log.info("code={}", code);
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setFrom(from);
-            mailMessage.setTo(address);
-            mailMessage.setSubject(subject);
-            mailMessage.setText(context);
-            // 真正的发送邮件操作
-            mailSender.send(mailMessage);
-            // 验证码由保存到session 优化为 缓存到Redis中，并且设置验证码的有效时间为 5分钟
-            redisTemplate.opsForValue().set( "forget:"+address, code, 5, TimeUnit.MINUTES);
-            return ResponseResult.successResult("验证码发送成功，请及时查看!");
-        }
-        return ResponseResult.errorResult(505,"验证码发送失败，请重新输入!");
 
-    }
+
 }
 
 
