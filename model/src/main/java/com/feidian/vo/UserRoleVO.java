0096dd7d374e08cc8e220c8eefe84ad4fe9056f2
space dandy <fsdkgldjg>
2023-08-02 14:08:29 +0800

    fix(admin):修改了查看所有权限角色的bug

--- model/src/main/java/com/feidian/vo/UserRoleVO.java
+++ model/src/main/java/com/feidian/vo/UserRoleVO.java
@@ -1,5 +1,12 @@
 package com.feidian.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRoleVO {
 
     private Long userId;
